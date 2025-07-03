74195d068fa42744bb0ae8c3fcae8d44d5ff3b24
Preampreda <bs21365@boripat.ac.th>
2025-07-03 16:19:08 +0700

    Update registor_db.php

--- registor_db.php
+++ registor_db.php
@@ -5,27 +5,47 @@
     $error = array();
 
     if (isset($_POST["reg-user"])){
-        $username = mysql_real_escape_string($c);
-        $email = mysql_real_escape_string($email);
-        $password = mysql_real_escape_string($password_1);
-        $password = mysql_real_escape_string($password_2);
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
+        $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
 
         if (empty($username)) {
-            array_push($error,"usrname is required")
+            array_push($error,"usrname is required");
 
         }
         if (empty($email)) {
-            array_push($error,"email is required")
+            array_push($error,"email is required");
         }
         if (empty($password_1)) {
-            array_push($error,"password is required")
+            array_push($error,"password is required");
         }
         if ($password_1 != $password_2){
-            array_push($error , "the tow passwords do not match")
+            array_push($error , "the two passwords do not match");
         }
         $user_check_query = "SELECT * FROM user WHERE username ='$username' OR email = '$email' " ;
-        $puery = mysql_query($conn , $user_check_query);
-        $result = mysql_fetch_assoc($query);
+        $query = mysqli_query($conn , $user_check_query);
+        $result = mysqli_fetch_assoc($query);
+
+        if ($result) {
+            if ($result['username'] === $username) {
+                array_push($error, "Username already exists");
+            }
+            if ($result['email'] === $email) {
+                array_push($error, "Email already exists");
+            }
+        
+        }
+        if (count($error) == 0 ) {
+            $password = md5($password_1);
+
+            $query = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
+            mysqli_query($conn, $sql);
+
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = 'You are now logged in';
+            header('location: index.php');
+        }
 
         
     }   
