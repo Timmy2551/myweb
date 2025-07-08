edaeb8b54a766b1b4ef40a9ea1a6c613b4aeacc5
Timmy <rsbmobile118@gmail.com>
2025-07-08 16:53:12 +0700

    Add files via upload

---
+++ shopee/form_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="form_login.css">
+</head>
+<body>
+
+    <div class="contain">
+    <h1> Login </h1>
+    <form action="" method = "POST" ></form>
+    <div class = "email_account">
+        <input  name = "email_account" type="email" placeholer="email" require>
+    </div>
+    <div class = "password_account">
+        <input  name = "password_account" type="password" placeholer="password" require>
+    </div>
+    <button>Login</button>
+    <a href=" #"></a>
+    </div>
+</body>
+</html>
