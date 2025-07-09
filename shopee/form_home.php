e99cebc16d49ab6964ca447530543b3278986337
Timmy <rsbmobile118@gmail.com>
2025-07-09 15:58:51 +0700

    Add files via upload

---
+++ shopee/form_home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="form_home.css">
+</head>
+<body>
+    <div class="head">
+    <a href="form_login.php" class ="call-buttonlogin"> Login  </a>
+    <p> บัญชี </p>
+    <div class="query">
+       <input type="text" name="query" placeholder="พิมพ์คำที่ต้องการค้นหา...">
+       <button type="submit">ค้นหา</button>
+    </div>
+    </div>
+
+    <div class="imagehome">
+        <div class="image">
+           <img src="https://m.media-amazon.com/images/I/51O5ILCiokL._UF1000,1000_QL80_.jpg" alt="NIKE" style="width: 300px; height: 470px; margin-left:290px; margin-top:5px ">
+        </div>
+        <div class="image2">
+           <img src="https://cdn3.f-cdn.com//files/download/202017283/IMG_20230510_224710_978.jpg?width=780&height=1103&fit=crop" alt="" style="width: 300px; height: 470px; margin-left:15px; margin-top:5px ">
+        </div>
+        <div class="image2">
+            <img src="https://cf.shopee.co.th/file/12ae4920bbbf908d75374172e0879949" alt="" style="width: 300px; height: 470px; margin-left:15px; margin-top:5px ">
+        </div>
+        <div class="image3">
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-poster-design-template-39a7eec2a3781230073fbf97ab800114_screen.jpg?ts=1738010986" alt="" style="width: 300px; height: 470px; margin-left:15px; margin-top:5px ">
+        </div>
+    </div>
+    </div>
+
+    <div class="menu">
+        <h1> food </h1>
+        <a href="product_1.php" class="call-button1">  ของหวาน </a>
+        <a href="#" class="call-button2">เมนูยอดนิยม</a>
+        <h2> ของใช้ </h2>
+        <a href="tel:+" class ="call-button3"> ของใช้ทั่วไป </a>
+        <a href="tel:+" class ="call-button4"> อุปกร it </a>
+    </div>
+    
+  
+
+</body>
+</html>
---
+++ shopee/product_1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procuct</title>
+    <link rel="stylesheet" href="product_1.css">
+</head>
+<body>
+<div class="head">
+    <a href="form_home.php" class ="call-buttonhome"> Home  </a>
+    <p> บัญชี </p>
+</div>
+<div class="menu">
+        <h1> food </h1>
+        <a href="product_1.php" class="call-button1">  ของหวาน </a>
+        <a href="#" class="call-button2">เมนูยอดนิยม</a>
+        <h2> ของใช้ </h2>
+        <a href="tel:+" class ="call-button3"> ของใช้ทั่วไป </a>
+        <a href="tel:+" class ="call-button4"> อุปกร it </a>
+        
+</div>
+
+
+    
+</body>
+</html>
